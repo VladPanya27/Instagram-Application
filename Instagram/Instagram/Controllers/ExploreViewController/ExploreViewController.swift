75d33721f18d858ab515971ae6b0d002a5f5f1b5
Vlad <vlad.ios.develop@gmail.com>
2022-04-09 10:38:13 +0300

    delete storyboard and added controllers

---
+++ Instagram/Instagram/Controllers/ExploreViewController/ExploreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by Vlad Panchenko on 09.04.2022.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
